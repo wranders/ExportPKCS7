9b8327cc5195790037634c72043181f93985121a
W Anders <w@doubleu.codes>
2021-01-03 07:55:09 -0600

    Updated buildiso script
    Added missing files and directories to the ignore flags. Built ISO now
    only contains module, installer, and license.

--- buildiso.sh
+++ buildiso.sh
@@ -2,8 +2,12 @@
 
 genisoimage \
     -input-charset utf-8 \
+    -m .git \
+    -m .gitignore \
     -m buildiso.sh \
+    -m Dockerfile \
     -m ExportPKCS7.iso \
+    -m README.md \
     -iso-level 3 \
     -o ExportPKCS7.iso \
     $PWD
